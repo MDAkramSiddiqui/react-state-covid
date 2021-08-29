373618ecaac4782e4dcff81c6fd68d023129615d
Mohammad Akram <40524561+MDAkramSiddiqui@users.noreply.github.com>
2021-08-30 02:01:44 +0530

    Feat :: added filter functionality

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Renew32 } from '@carbon/icons-react';
 import { Button } from 'carbon-components-react';
 import { useDispatch, useSelector } from 'react-redux';
+import { debounce } from 'lodash';
 
 import constants from '../constants';
 import Loader from './loader';
@@ -11,21 +12,42 @@ import updateStatesData from '../actions/states';
 
 function App() {
     const dispatch = useDispatch();
-
-    const [isDataLoading, setIsDataLoading] = useState(true);
     const covidStatesData = useSelector((store) => store.states);
+    const [isDataLoading, setIsDataLoading] = useState(true);
+    const [statesData, setStatesData] = useState(covidStatesData);
 
     const fetchCovidData = () => {
         fetch(
             `${constants.global.BASE_API_URI}${constants.global.COVID_STATES_DATA_API}`,
         )
             .then((response) => response.json())
-            .then((data) => {
-                if (data.status === 'success') {
-                    dispatch(updateStatesData(data.data));
+            .then((response) => {
+                if (response.status === 'success') {
+                    dispatch(updateStatesData(response.data));
+                    setStatesData(response.data);
                 }
                 setIsDataLoading(false);
             });
+    };
+
+    const filterList = (stateQuery) => {
+        if (!stateQuery) {
+            setStatesData(covidStatesData);
+            return;
+        }
+
+        const queries = stateQuery
+            .split(',')
+            .map((q) => q.trim().toLowerCase());
+        const filteredStatesData = [];
+        covidStatesData.forEach((state) => {
+            const stateName = state.name.toLowerCase();
+            if (queries.some((q) => stateName.includes(q))) {
+                filteredStatesData.push(state);
+            }
+        }, []);
+
+        setStatesData(filteredStatesData);
     };
 
     useEffect(() => {
@@ -35,14 +57,14 @@ function App() {
     return (
         <div className="bx--grid bx--grid--full-width">
             <div className="bx--row wrapper">
-                <SearchBar />
+                <SearchBar onChange={debounce(filterList, 500)} />
                 <Button hasIconOnly iconDescription="Refresh" size="md">
                     <Renew32 />
                 </Button>
             </div>
             {isDataLoading && <Loader />}
             {!isDataLoading &&
-                covidStatesData.map((data, index) => (
+                statesData.map((data, index) => (
                     <StateCard stateData={data} key={index} />
                 ))}
         </div>
--- src/components/searchBar/searchBarComponent.js
+++ src/components/searchBar/searchBarComponent.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './searchBar.css';
 import { Search } from 'carbon-components-react';
 
-function SearchBar() {
+function SearchBar({ onChange }) {
+    const [statesQuery, setStatesQuery] = useState('');
+
+    const handleStateQueryChange = (e) => {
+        setStatesQuery(e.target.value);
+        onChange(e.target.value);
+    };
+
     return (
         <Search
+            value={statesQuery}
+            onChange={(e) => handleStateQueryChange(e)}
             className="inline"
             placeholder="Search, ex. Delhi, Kerala, All etc."
             labelText="Search"
         />
     );
 }
+
+SearchBar.propTypes = {
+    onChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
