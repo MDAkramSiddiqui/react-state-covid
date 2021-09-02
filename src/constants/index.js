0d0f99b6bfd61eaafe7360e731f085cb030d502c
Mohammad Akram <40524561+MDAkramSiddiqui@users.noreply.github.com>
2021-09-03 00:27:47 +0530

    Fix :: added correct endpoint

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     global: {
-        BASE_API_URI: 'https://node-inshorts-covid-api.herokuapp.com/api/v1/covid/states',
+        BASE_API_URI: 'https://node-inshorts-covid-api.herokuapp.com/api/v1',
         COVID_STATES_DATA_API: '/covid/states',
         APP_NAME: 'react-state-covid',
     },
