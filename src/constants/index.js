72c4da56284a15008a1acded69aab13f209d2b45
Mohammad Akram <40524561+MDAkramSiddiqui@users.noreply.github.com>
2021-09-03 00:19:53 +0530

    Feat :: added correct endpoint

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     global: {
-        BASE_API_URI: 'http://localhost:8000/api/v1',
+        BASE_API_URI: 'https://node-inshorts-covid-api.herokuapp.com/api/v1/covid/states',
         COVID_STATES_DATA_API: '/covid/states',
         APP_NAME: 'react-state-covid',
     },
